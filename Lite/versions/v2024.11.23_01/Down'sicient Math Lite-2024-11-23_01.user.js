63828fec754f6ae69e2a9fb1b3ccbf41578e88d6
RDF People's Libertation Front <contact@tach.eu.org>
2024-11-29 18:12:59 +0800

    Update Script Header

--- Lite/versions/v2024.11.23_01/Down'sicient Math Lite-2024-11-23_01.user.js
+++ Lite/versions/v2024.11.23_01/Down'sicient Math Lite-2024-11-23_01.user.js
@@ -4,6 +4,11 @@
 // @version      2024-11-23_01
 // @description  Freiheit oder Tod!
 // @author       People's Libertation Front
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM.getValue
+// @grant        GM.setValue
+// @require      https://openuserjs.org/src/libs/sizzle/GM_config.min.js
 // @match        http*://m.afficienta.cn*
 // @icon         https://s1.locimg.com/2024/11/12/a417504056ddc.png
 // @homepage     https://downs.tach.eu.org/
